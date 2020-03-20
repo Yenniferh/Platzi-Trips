4a73ca77800efa2d6b38cc5a37fcdbc2134efe65
Yennifer Herrera <yenniferh@uninorte.edu.co>
2020-03-19 23:43:21 -0500

    Cupertino Tab Scaffold

---
+++ lib/platzi_trips_cupertino.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'home_trips.dart';
+import 'search_trips.dart';
+import 'profile_trips.dart';
+
+class PlatziTripsCupertino extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home), title: Text("")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search), title: Text("")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person), title: Text("")),
+        ],
+        activeColor: Colors.indigo,),
+
+
+        //ignore: Missing_return
+        tabBuilder: (BuildContext context, int index) {
+          switch (index) {
+            case 0:
+              return CupertinoTabView(
+                builder: (BuildContext context) => HomeTrips(),
+              );
+              break;
+            case 1:
+              return CupertinoTabView(
+                builder: (BuildContext context) => SearchTrips(),
+              );
+              break;
+            case 2:
+              return CupertinoTabView(
+                builder: (BuildContext context) => ProfileTrips(),
+              );
+              break;
+          }
+        },
+      ),
+    );
+  }
+}
